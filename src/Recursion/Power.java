bd50f956a94c5530ab5c927cf1f8cd74f04445fa
DELL <shubham13.ss11@gmail.com>
2022-06-24 19:08:44 +0530

    24/6 Recursion and few other probs

--- src/Recursion/Power.java
+++ src/Recursion/Power.java
@@ -3,6 +3,16 @@ package Recursion;
 import java.sql.SQLOutput;
 
 public class Power {
+
+    public static int fibonacci(int n){
+        if(n==0)return 0;
+        if(n==1)return 1;
+        int ans= fibonacci(n-1)+fibonacci(n-2);
+        //System.out.print(ans+"-");
+        return ans;
+
+    }
+
     public static int powerOf2(int n){
         //base case
         if(n==0)return 1;
@@ -17,5 +27,16 @@ public class Power {
         System.out.println(n);
         return reverseCount(n-1);
     }
+
+    public static int reachHome(int src , int dest){
+        if(src==dest){
+            System.out.println("Reached Home");
+            return 1;
+        }
+        System.out.println("Source:"+ src+"\t"+"Destination:"+dest);
+        reachHome(src+1,dest);
+        return 0;
+    }
+
 
 }
